be4abb0f07e4dea4074f6ea5d8c66855e57ea550
Joe Watson <joe_watson@mail.com>
2016-05-11 07:27:43 +0100

    Closer to final

---
+++ README.md
@@ -0,0 +1,8 @@
+
+TODO
+
+* Check tweet button works (new frame)
+* Finalize style for web
+* Nicer fallback if cannot load twitter.com?
+* Check Mobile version
+* Add non-query version - pre-processor on html? or js?
--- src/random-quote.js
+++ src/random-quote.js
@@ -2,23 +2,55 @@
 
 $(document).ready(() => {
 
-  // Put the first quote in place
   updateQuote();
 
-  // Add trigger for new quote onto new quote button
-  $("#newQuote").on("click", updateQuote);
-
-
+  $("#new-quote-button").on("click", updateQuote);
 
 });
 
-
-console.log("In js");
-
 function updateQuote() {
-  console.log("In updateQuote");
+
+  let newQuote ="";
+
   $.getJSON("http://api.icndb.com/jokes/random", (json) => {
-    console.log("Got", json);
-    $(".quote").text(json.value.joke);
+
+    newQuote = json.value.joke;
+    writeQuote(newQuote || newOfflineQuote());
+
+  }).fail(() => {
+
+    writeQuote(newOfflineQuote());
+
   });
+
 }
+
+
+function writeQuote(quote) {
+
+  $(".quote-text").html(quote);
+  $("#tweet-button-anchor").prop("href", encodeURI(tweetUrlStem + quote));
+
+}
+
+
+function newOfflineQuote() {
+
+  let i = Math.floor(Math.random() *offlineQuotes.length);
+
+  return offlineQuotes[i];
+
+}
+
+
+const tweetUrlStem = "https://twitter.com/intent/tweet?hashtags=chuck&related=freecodecamp&text=";
+
+
+const offlineQuotes = [
+  "In the Words of Julius Caesar, &quot;Veni, Vidi, Vici, Chuck Norris&quot;. " +
+    "Translation: I came, I saw, and I was roundhouse-kicked inthe face by Chuck Norris.",
+  "A handicapped parking sign does not signify that this spot is for handicapped people. " +
+    "It is actually in fact a warning, that the spot belongs to Chuck Norris " +
+    "and that you will be handicapped if you park there.",
+  "Chuck Norris drives an ice cream truck covered in human skulls."
+];
